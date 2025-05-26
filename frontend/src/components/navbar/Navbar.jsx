ac0d0c68f6467f8ab84a6da057cf8578f9a8d8d2
PedroHSGAraujo <pedrohsgaraujo@gmail.com>
2025-05-26 00:39:47 -0300

    responsividade

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,14 +1,20 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import './Navbar.css'
 import logo from '../assets/logo.png'
 import cart_icon from '../assets/cart_icon.png'
 import { Link } from 'react-router-dom';
 import { ShopContext } from '../../context/ShopContext';
+import nav_dropdown from '../assets/nav_dropdown.png'
 
 export const Navbar = () => {
 
     const [menu, setMenu] = useState("loja");
     const { getTotalCartItems } = useContext(ShopContext);
+    const menuRef = useRef();
+    const dropdown_toggle = (e) => {
+        menuRef.current.classList.toggle('nav-menu-visible');
+        e.target.classList.toggle('open');
+    }
 
     return (
         <div className='navbar'>
@@ -16,7 +22,8 @@ export const Navbar = () => {
                 <img src={logo} alt="" />
                 <p>MAHARA</p>
             </div>
-            <ul className='nav-menu'>
+            <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+            <ul ref={menuRef} className='nav-menu'>
                 <li onClick={() => { setMenu("loja") }}><Link style={{ textDecoration: 'none', color: 'inherit' }} to='/'>Loja</Link>{menu === "loja" ? <hr /> : <></>}</li>
                 <li onClick={() => { setMenu("colares") }}><Link style={{ textDecoration: 'none', color: 'inherit' }} to='/colares'>Colares</Link>{menu === "colares" ? <hr /> : <></>}</li>
                 <li onClick={() => { setMenu("pulseiras") }}><Link style={{ textDecoration: 'none', color: 'inherit' }} to='/pulseiras'>Pulseiras</Link>{menu === "pulseiras" ? <hr /> : <></>}</li>
